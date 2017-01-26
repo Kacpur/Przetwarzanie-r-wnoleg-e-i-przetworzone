5fec8eea6d2a392dc305fcf10f9f5a258a287b85
napior <knapior93@gmail.com>
2017-01-26 23:45:26 +0100

    podstawa gui

--- app/src/main/java/tak/bo/irc/ircclient/ChatActivity.java
+++ app/src/main/java/tak/bo/irc/ircclient/ChatActivity.java
@@ -1,8 +1,11 @@
 package tak.bo.irc.ircclient;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,25 +15,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ChatActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private String nick;
+    private String addres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        nick = (String) getIntent().getExtras().get(LoginActivity.NICK);
+        addres = (String) getIntent().getExtras().get(LoginActivity.ADRESS);
+
         setContentView(R.layout.activity_chat);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,22 +82,108 @@ public class ChatActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_users) {
+            userList(null);
+        } else if (id == R.id.nav_channals) {
+            channelList(null);
+        } else if (id == R.id.nav_add) {
+            createChannel();
+        } else {
+            if(item.getIcon().getConstantState().equals(
+                    getResources().getDrawable(R.drawable.ic_user).getConstantState())){
+                sendPrivateMsg(item.getTitle().toString());
+            } else if(item.getIcon().getConstantState().equals(
+                    getResources().getDrawable(R.drawable.ic_channel).getConstantState())) {
+                dodaj("System", item.getTitle().toString());
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void userList(View v) {
+        startActivity(new Intent(this, UserListActivity.class));
+    }
+
+    public void channelList(View v) {
+        startActivity(new Intent(this, ChanneListActivity.class));
+    }
+
+    public void wyslij(View v) {
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        dodaj(this.nick, editText.getText().toString());
+        addToNavigation(editText.getText().toString(), true);
+    }
+
+    public void dodaj(String nick, String msg) {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.content_chat);
+        linearLayout.addView(new MsgView(this, nick, msg).getForm());
+    }
+
+    public void addToNavigation(String title, boolean user){
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        MenuItem newItem = navigationView.getMenu().add(title);
+        if(user) {
+            newItem.setIcon(R.drawable.ic_user);
+        } else {
+            newItem.setIcon(R.drawable.ic_channel);
+        }
+    }
+
+    private void createChannel(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.addChanell);
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String m_Text = input.getText().toString();
+                addToNavigation(m_Text,false);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void sendPrivateMsg(final String who){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        StringBuilder title = new StringBuilder()
+                .append(getResources().getString(R.string.sendPrivateMsg))
+                .append(" ")
+                .append(who);
+        builder.setTitle(title.toString());
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String m_Text = input.getText().toString();
+                dodaj(who,m_Text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
 }
--- app/src/main/java/tak/bo/irc/ircclient/LoginActivity.java
+++ app/src/main/java/tak/bo/irc/ircclient/LoginActivity.java
@@ -1,13 +1,17 @@
 package tak.bo.irc.ircclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String NICK = "NICK";
+    public static final String ADRESS = "ADDRESS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +19,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void login(View v) {
+        EditText nick = (EditText) findViewById(R.id.editTextNick);
+        EditText address = (EditText) findViewById(R.id.editTextAdress);
+        Intent chat = new Intent(this, ChatActivity.class);
+//        startActivity(new Intent(this, ChatActivity.class));
+        chat.putExtra(LoginActivity.NICK, nick.getText().toString());
+        chat.putExtra(LoginActivity.ADRESS, address.getText().toString());
+        startActivity(chat);
     }
 
 }
